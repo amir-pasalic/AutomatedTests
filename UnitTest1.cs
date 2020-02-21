58b89c6a8c60f114c25b57bada7581107892b851
amirpasalic11@outlook.com <amirpasalic11@outlook.com>
2020-02-21 16:06:13 +0100

    comment

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,18 +1,46 @@
 using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Linq;
 
 namespace AutomatedTests
 {
     public class Tests
     {
+        ChromeDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver(".");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Close();
         }
 
         [Test]
-        public void Test1()
+        public void CanOpenGithub()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://github.com");
+
+            var title = driver.FindElement(By.CssSelector("h1")).Text;
+
+            Assert.AreEqual(title, "Built for developers");
+        }
+
+
+        [Test]
+        public void ContainsAllMyRepositories()
+        {
+            driver.Navigate().GoToUrl("https://github.com/amir-pasalic?tab=repositories");
+
+            var repoLiElements = driver.FindElements(By.CssSelector("[data-filterable-for=\"your-repos-filter\"] > li"));
+
+            Assert.AreEqual(1, repoLiElements.Count);
         }
     }
 }
+//testtest
